37204dff161bf51071587858e8c4b77b2aeefe31
gem-jashanveer <jashanveer.singh@geminisolutions.in>
2022-03-22 15:58:43 +0530

    Project setup

---
+++ src/test/java/com/gemini/Clovek/ClovekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemini.Clovek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClovekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
